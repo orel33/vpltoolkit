ea36d65f9ab1bb8c8fa5a9c06ec1e34835673d00
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-10-17 01:38:19 +0200

    minor

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ function model_setenv()
     # basic environment
     VERSION="1.0"
     [ -z "$MODE" ] && echo "⚠ MODE variable is not defined!" && exit 0
+    [ -z "$REPOSITORY" ] && echo "⚠ REPOSITORY variable is not defined!" && exit 0
     [ -z "$EXO" ] && echo "⚠ EXO variable is not defined!" && exit 0
     [ -z "$DEBUG" ] && DEBUG=0
     [ -z "$VERBOSE" ] && VERBOSE=0
@@ -13,6 +14,7 @@ function model_setenv()
     rm -f $HOME/env.sh
     echo "VERSION=$VERSION" >> $HOME/env.sh
     echo "MODE=$MODE" >> $HOME/env.sh
+    echo "REPOSITORY=$REPOSITORY" >> $HOME/env.sh
     echo "EXO=$EXO" >> $HOME/env.sh
     echo "DEBUG=$DEBUG" >> $HOME/env.sh
     echo "VERBOSE=$VERBOSE" >> $HOME/env.sh
