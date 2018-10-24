8d04a8a54f6c4946c6102e1cc5f6399f133b2c0b
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-10-24 22:55:28 +0200

    minor

--- local_run.sh
+++ local_run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 VPLMODEL="https://github.com/orel33/vplmodel.git"
 MODE="RUN"
-# REPOSITORY="https://github.com/orel33/vplmodel.git"
-# BRANCH="demo"
-EXO="hello"
 DEBUG=1
 VERBOSE=1
 RUNDIR=$(mktemp -d)
 
 cd $RUNDIR && git clone $VPLMODEL &> /dev/null && cd -
 source $RUNDIR/vplmodel/toolkit.sh
+# ln -sf $RUNDIR/vplmodel/vpl_execution $HOME/vpl_execution
 
-DOWNLOAD "https://github.com/orel33/vplmodel.git" "demo" "hello"
+EXO="hello"
+DOWNLOAD "https://github.com/orel33/vplmodel.git" "demo" $EXO
 START
 
 # explicit run of vpl_execution
