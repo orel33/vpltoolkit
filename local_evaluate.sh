e881e09c45d7e1297f278514a0743bccc5089c42
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-10-27 00:03:37 +0200

    bug correction

--- local_evaluate.sh
+++ local_evaluate.sh
@@ -3,7 +3,6 @@
 EXO=$1
 INPUTDIR=$2
 RUNDIR=$(mktemp -d)
-MODE="RUN"
 ( cd $RUNDIR && git clone "https://github.com/orel33/vpltoolkit.git" &> /dev/null )
 source $RUNDIR/vpltoolkit/toolkit.sh
 DOWNLOAD "https://github.com/orel33/vpltoolkit.git" "demo" $EXO
