851ee683c4b738888d6fd8a7d236466b6b860527
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-10-26 09:59:03 +0200

    major

--- toolkit.sh
+++ toolkit.sh
@@ -137,7 +137,7 @@ function DOWNLOAD() {
 ### COPY INPUTS ###
 
 function COPYINPUTS() {
-    ECHOV "Inputs: $@"
+    ECHOV "INPUTS=\"$@\""
     mkdir -p $RUNDIR/inputs
     for FILE in "$@" ; do
         [ ! -f $FILE ] && ECHO "⚠ Input file \"$FILE\" is missing!" && exit 0
