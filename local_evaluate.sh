772a95700ced9a7c02560df65e025046a5c744a9
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-10-26 23:51:56 +0200

    add local evaluate script

---
+++ local_evaluate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+[ $# -ne 1 -a $# -ne 2 ] && echo "⚠ Usage: $0 <exo> [<inputdir>]" && exit 0
+EXO=$1
+INPUTDIR=$2
+RUNDIR=$(mktemp -d)
+MODE="RUN"
+( cd $RUNDIR && git clone "https://github.com/orel33/vpltoolkit.git" &> /dev/null )
+source $RUNDIR/vpltoolkit/toolkit.sh
+DOWNLOAD "https://github.com/orel33/vpltoolkit.git" "demo" $EXO
+START_OFFLINE $INPUTDIR
