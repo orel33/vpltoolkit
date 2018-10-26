e87de90770bb1958409093a221a2ecf255635753
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-10-26 17:15:49 +0200

    major

--- local_run.sh
+++ local_run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+[ $# -ne 2 ] && echo "⚠ Usage: $0 <exo> <inputdir>" && exit 0
+
+EXO=$1
+INPUTDIR=$2
 
 VPLMODEL="https://github.com/orel33/vplmodel.git"
 RUNDIR=$(mktemp -d)
 MODE="RUN"
-DEBUG=1
-VERBOSE=1
+# DEBUG=1
+# VERBOSE=1
 
 ( cd $RUNDIR && git clone $VPLMODEL &> /dev/null )
 source $RUNDIR/vplmodel/toolkit.sh
 
-EXO="mycat"
 DOWNLOAD "https://github.com/orel33/vplmodel.git" "demo" $EXO
-START_OFFLINE $1
+START_OFFLINE $INPUTDIR
