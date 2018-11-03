559c16fc06d06085ab598e98408bcc275e66641b
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-11-03 10:42:33 +0100

    move start routines in start.sh

--- local_eval.sh
+++ local_eval.sh
@@ -4,6 +4,6 @@ EXO=$1
 INPUTDIR=$2
 RUNDIR=$(mktemp -d)
 ( cd $RUNDIR && git clone "https://github.com/orel33/vpltoolkit.git" &> /dev/null )
-source $RUNDIR/vpltoolkit/toolkit.sh
+source $RUNDIR/vpltoolkit/start.sh
 DOWNLOAD "https://github.com/orel33/vpltoolkit.git" "demo" $EXO
 START_OFFLINE $INPUTDIR
--- vpl_evaluate.sh
+++ vpl_evaluate.sh
@@ -2,7 +2,7 @@
 rm -f $0
 RUNDIR=$(mktemp -d)
 ( cd $RUNDIR && git clone "https://github.com/orel33/vpltoolkit.git" &> /dev/null )
-source $RUNDIR/vpltoolkit/toolkit.sh
+source $RUNDIR/vpltoolkit/start.sh
 EXO="hello"
 DOWNLOAD "https://github.com/orel33/vpltoolkit.git" "demo" $EXO
 START_ONLINE
