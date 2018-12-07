0da5eea833428dbb9e85b9782f6923c689f9164d
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-12-07 15:47:41 +0100

    start to test docker

--- local_run.sh
+++ local_run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-[ $# -ne 1 -a $# -ne 2 ] && echo "⚠ Usage: $0 <exo> [<inputdir>]" && exit 0
-EXO=$1
+[ $# -ge 2 ] && echo "⚠ Usage: $0 <exo> <inputdir> <...>" && exit 0
+EXO=$1 # GIT SUBDIR
 INPUTDIR=$2
+ARGS="${@:3}"
+TKGIT="https://github.com/orel33/vpltoolkit.git"
+TKBRANCH="master"
 RUNDIR=$(mktemp -d)
-( cd $RUNDIR && git clone "https://github.com/orel33/vpltoolkit.git" &> /dev/null )
+echo "RUNDIR=$RUNDIR"
+( cd $RUNDIR && git clone $TKGIT -b $TKBRANCH &> /dev/null )
 source $RUNDIR/vpltoolkit/start.sh
 DOWNLOAD "https://github.com/orel33/vpltoolkit.git" "demo" $EXO
-START_OFFLINE $INPUTDIR
+START_OFFLINE $INPUTDIR  $ARGS
