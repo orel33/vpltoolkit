b77aeeaa9cb21481c34a8e4899de4ccfc67c0f77
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2019-04-19 15:38:32 +0200

    minor

--- start.sh
+++ start.sh
@@ -125,7 +125,13 @@ function START_ONLINE()
     [ -z "$MODE" ] && echo "⚠ MODE variable is not defined!" && exit 0
     source $HOME/vpl_environment.sh
     mkdir -p $RUNDIR/inputs
-    [ ! -z "$VPL_SUBFILES" ] && ( cd $HOME && cp $VPL_SUBFILES $RUNDIR/inputs ) # FIXME: here bug if file contains spaces
+    # [ ! -z "$VPL_SUBFILES" ] && ( cd $HOME && cp $VPL_SUBFILES $RUNDIR/inputs ) # FIXME: here bug if file contains spaces
+    for var in ${!VPL_SUBFILE@} ; do
+        # $var => variable name 
+        # ${!var} => variable value
+        cp "${!var}" $RUNDIR/inputs
+    done
+
     INPUTS=\"$(cd $RUNDIR && find -L inputs -maxdepth 1 -type f | xargs)\"
     # INPUTS=$(echo -n \" && cd $RUNDIR && find inputs -maxdepth 1 -type f | xargs && echo -n \")
     CHECKENV
