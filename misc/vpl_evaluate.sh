fca43b0dc926c2aa2264d16cb92d926f0fa03bab
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2019-05-09 15:39:11 +0200

    remove useless misc

--- misc/vpl_evaluate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -f $0 # for security issue
-RUNDIR=$(mktemp -d)
-( cd $RUNDIR && git clone "https://github.com/orel33/vpltoolkit.git" -b "3.0" &> /dev/null )
-[ ! $? -eq 0 ] && echo "⚠ Fail to download VPL Toolkit!" && exit 0
-source $RUNDIR/vpltoolkit/start.sh
-DOWNLOAD "https://github.com/orel33/vpltoolkit.git" "demo" "hello"
-START_ONLINE
