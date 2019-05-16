4e7218e75165bfbb60581c4d651c6df35ba4607b
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2019-05-16 17:52:36 +0200

    update docker with user permission

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker build -t "orel33/mydebian:latest" . && docker push "orel33/mydebian:latest"
+
+docker build -t "orel33/mydebian:user" -f Dockerfile.user . && docker push "orel33/mydebian:user"
