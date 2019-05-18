ba1ebc3004c65f6db726c660ffdcda189dc1f429
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2019-05-18 10:09:11 +0200

    docker

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker build -t "orel33/mydebian:latest" . && docker push "orel33/mydebian:latest"
-
 docker build -t "orel33/mydebian:user" -f Dockerfile.user . && docker push "orel33/mydebian:user"
