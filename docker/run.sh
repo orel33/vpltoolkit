563cf20d7988899259c0cce71f6927f02fdff52a
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2019-09-23 16:29:25 +0200

    clear docker files

--- docker/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOCKEROPT=""
-DOCKEROPT="$DOCKEROPT -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker"
-docker run -it --rm  $DOCKEROPT orel33/mydebian bash
-
-# EOF
-
