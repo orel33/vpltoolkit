9cbf2ea52f43d4ce0dfba28b2188ce944d90dc4a
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2021-10-07 14:32:21 +0200

    enforce the build of certain docker layers

--- docker/minivpl-ubuntu/build-moodle.sh
+++ docker/minivpl-ubuntu/build-moodle.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+date > build-date.txt # use to enforce the build of certain layers
 docker build -t "orel33/minivpl-ubuntu" . && docker --config="/root/.docker.orel/" push "orel33/minivpl-ubuntu"
--- docker/minivpl-ubuntu/build.sh
+++ docker/minivpl-ubuntu/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 OPT="$*" # --no-cache
 echo "OPT=$OPT"
+date > build-date.txt # use to enforce the build of certain layers
 docker build $OPT -t "orel33/minivpl-ubuntu" . && docker push "orel33/minivpl-ubuntu"
 
