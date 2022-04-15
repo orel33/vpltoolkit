7796cc7cda13357738b66c6f59ceffd2283394f0
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2022-04-15 13:33:01 +0200

    mv docker ilages

--- docker/minivpl-ubuntu/build-moodle.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-date > build-date.txt # use to enforce the build of certain layers
-docker build -t "orel33/minivpl-ubuntu" . && docker --config="/root/.docker.orel/" push "orel33/minivpl-ubuntu"
--- docker/minivpl-ubuntu/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-OPT="$*" # --no-cache
-echo "OPT=$OPT"
-date > build-date.txt # use to enforce the build of certain layers
-docker build $OPT -t "orel33/minivpl-ubuntu" . && docker push "orel33/minivpl-ubuntu"
-
